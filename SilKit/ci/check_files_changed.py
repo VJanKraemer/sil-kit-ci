93f70f9df1ecba6d21f6bdac7fc030f0d624b6b3
Jan Kraemer <jan.kraemer@vector.com>
2025-10-23 09:28:23 +0200

    dco: Add dco check

--- SilKit/ci/check_files_changed.py
+++ SilKit/ci/check_files_changed.py
@@ -27,6 +27,49 @@ def die(status, fmt, *args):
     log(ERROR_PREFIX + fmt, *args)
     sys.exit(status)
 
+def check_run_build(url: str):
+    run_builds = "false"
+    files_url = url + '/files'
+    r = requests.get(files_url, verify=False)
+
+    for fileObject in r.json():
+
+        file_path = fileObject["filename"];
+        file = file_path.split(sep="/")[-1]
+
+        if file not in exceptional_files:
+            run_builds = "true"
+            break
+
+    log("Builds should run: {}".format(run_builds))
+
+    if isCI != None:
+        log("Setting GITHUB_OUTPUT!")
+        with open(os.environ["GITHUB_OUTPUT"], 'a') as f:
+            print("run_builds={}".format(run_builds), file=f)
+
+def check_dco(url: str):
+    commits_url = url + '/commits'
+
+    r = requests.get(commits_url, verify=False)
+
+    for commitObject in r.json():
+        committer = commitObject["commit"]["committer"]
+        author = commitObject["commit"]["author"]
+        message = commitObject["commit"]["message"]
+
+        if committer["email"] != author["email"] or committer["name"] != author["name"]:
+                 die(66, f'Cannot merge, committer {commiter["name"]} is not equal to author {author["name"]}')
+
+
+        if not "fixup" in message:
+            sign_off = f'Signed-off-by: {author["name"]} <{author["email"]}>'
+            if not sign_off in message:
+                warn("No sign off in non fixup mesage, please sign off your commits to accept the DCO\nhttps://developercertificate.org/")
+                die("No sign off in non fixup message, Merging Blocked")
+            else:
+                print(f'Commit {commitObject["sha"][0:7]} properly signed-off')
+
 # File Set
 exceptional_files = {'README.rst', 'latest.md', 'LICENSE', 'CONTRIBUTING.md',
                      'SilKitVersion.cmake'}
@@ -37,26 +80,10 @@ parser.add_argument('repo', type=str)
 parser.add_argument('PR', type=str)
 args = parser.parse_args()
 
-run_builds = "false"
-
-url = 'https://api.github.com/repos/' + args.repo + '/pulls/' + args.PR + '/files'
+url = 'https://api.github.com/repos/' + args.repo + '/pulls/' + args.PR
 
 log("Checking at {}".format(url))
 
-r = requests.get(url, verify=False)
+check_dco(url)
+check_run_build(url)
 
-for fileObject in r.json():
-
-    file_path = fileObject["filename"];
-    file = file_path.split(sep="/")[-1]
-
-    if file not in exceptional_files:
-        run_builds = "true"
-        break
-
-log("Builds should run: {}".format(run_builds))
-
-if isCI != None:
-    log("Setting GITHUB_OUTPUT!")
-    with open(os.environ["GITHUB_OUTPUT"], 'a') as f:
-        print("run_builds={}".format(run_builds), file=f)
